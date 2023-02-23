0ba65b0fc109f9f1b5874a2359c2ba9644a00d17
ACortex <53233006+ArturCortez@users.noreply.github.com>
2023-02-23 16:59:39 +0000

    added api(dolar) rascunho

--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,17 @@
-function myFunction() {
-    console.log("Hello from helper.js!");
+export function getExchangeRate(apiUrl, appId) {
+    const url = `${apiUrl}/latest.json?app_id=${appId}&base=USD&symbols=BRL`;
+    
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data.rates.BRL)
+      //.then(usdToBrlRate => {
+       // return usdToBrlRate;
+      //})
+      .catch(error => {
+        console.log(`Error fetching exchange rate: ${error}`);
+        throw error;
+      });
   }
   
+
+ 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-myFunction();
+
+
+import { getExchangeRate } from "./helper.js";
+
+const btnConverter = document.getElementById("btn-converter");
+
+// script.js
+//exchangeRateButton.addEventListener('click', () => {
+btnConverter.addEventListener('click', () => {
+
+
+    const appId = 'fc25242735d34e3da284c78de0776e75';
+    const resultadoElement = document.getElementById("resultado");
+    const valorInserido = document.querySelector("valueToConvert");
+    
+
+  // Usage:
+  getExchangeRate("https://openexchangerates.org/api", appId)  
+    .then(rate => {
+        console.log(valorInserido * 2)
+        const formattedRate = rate
+        resultadoElement.textContent = "USD to BRL value is " + 3 * formattedRate;
+    })  
+    .catch(error => console.log(`Error: ${error}`));
+
+  });
+
+  
+
+
+
+
