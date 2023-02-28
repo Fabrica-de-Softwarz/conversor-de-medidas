494e33cfe13642feab7a46bd6eb942fc13e0d032
ACortex <53233006+ArturCortez@users.noreply.github.com>
2023-02-28 21:53:31 +0000

    Incrementado o processo de conversão

--- js/helper.js
+++ js/helper.js
@@ -12,6 +12,29 @@ export function getExchangeRate(apiUrl, appId) {
         throw error;
       });
   }
-  
 
- 
+  async function calculate(payload) {
+    const BASE_URL = 'https://arturcortez.pythonanywhere.com/calc';
+   
+    
+    const response = await fetch(`${BASE_URL}?${new URLSearchParams(payload)}`);
+    const json_values = await response.json();
+    
+    const rq_input = json_values.input;
+    const timestamp = json_values.timestamp;
+    const character_count = json_values.character_count;
+    const number1 = parseInt(json_values.number1);
+    const number2 = parseInt(json_values.number2);
+    const result = number1 * number2;
+    
+    console.log(`Input is ${rq_input}`);
+    console.log(`Time is ${timestamp}`);
+    console.log(`Character count is ${character_count}`);
+    console.log(`Numbers are ${number1} and ${number2}`);
+    console.log(`Result is ${result}`);
+    
+    return {input: rq_input, time: timestamp, characterCount: character_count, numbers: [number1, number2], result: result};
+  }
+  
+  export { calculate };
+  
--- js/script.js
+++ js/script.js
@@ -2,31 +2,61 @@
 
 import { getExchangeRate } from "./helper.js";
 
+import { calculate } from "./helper.js";
+
+const myForm = document.querySelector('#mainForm');
+
 const btnConverter = document.getElementById("btn-converter");
+
+
+ 
+  const valorInserido = document.querySelector("#valueToConvert");
+  const valorInseridoFloat = parseFloat(valorInserido.textContent);
+  console.log(parseFloat(valorInseridoFloat));
+
+
 
 // script.js
 //exchangeRateButton.addEventListener('click', () => {
-btnConverter.addEventListener('click', () => {
+btnConverter.addEventListener('click', async () => {
 
+    //Moeda a Converter
+    const selectElementConvertFromValue = document.getElementById("convertFromValueType");
+    const selectedOptionConvertFromValue = selectElementConvertFromValue.options[selectElementConvertFromValue.selectedIndex];
+    const selectedValueConvertFromValue = selectedOptionConvertFromValue.value;
+    console.log(selectedValueConvertFromValue);
+  
+    //Moeda pós-conversão
+    const selectElementToConvertTo = document.getElementById("convertToValueType");
+    const selectedOptionToConvertTo = selectElementToConvertTo.options[selectElementToConvertTo.selectedIndex];
+    const selectedValueToConvertTo = selectedOptionToConvertTo.value;
+    console.log(selectedValueToConvertTo);
+
+
+    const valueInserted = document.getElementById("valueToConvert").value;
+    console.log(valueInserted);
 
     const appId = 'fc25242735d34e3da284c78de0776e75';
     const resultadoElement = document.getElementById("resultado");
-    const valorInserido = document.querySelector("valueToConvert");
-    
 
+    const payload = {input: 'Datetime', number1: '11', number2: '20'};
+
+    const result = await calculate(payload);
+
+    console.log(result);
+
+
+    
+/*
   // Usage:
   getExchangeRate("https://openexchangerates.org/api", appId)  
     .then(rate => {
-        console.log(valorInserido * 2)
+        console.log(parseFloat(valorInseridoFloat))
         const formattedRate = rate
         resultadoElement.textContent = "USD to BRL value is " + 3 * formattedRate;
     })  
     .catch(error => console.log(`Error: ${error}`));
-
+*/
   });
 
-  
-
-
-
-
+   
